bfa1628838cf8fc31a7431bee3fd42b2b15230ad
romanskz <skubiszewski.roman@gmail.com>
2018-12-04 16:43:08 +0100

    Suite du code

--- Code/ItemRank.py
+++ Code/ItemRank.py
@@ -1,22 +1,8 @@
 import numpy as np
 
-matrice = np.matrix([
-   # 1  2  3  4  5  6  7  8  9  10
-    [0, 1, 1, 0, 1, 0, 1, 1, 1, 1], #1
-    [1, 0, 0, 1, 1, 0, 1, 1, 0, 0], #2
-    [1, 0, 0, 0, 1, 1, 1, 1, 1, 0], #3
-    [0, 1, 0, 0, 1, 0, 0, 0, 0, 0], #4
-    [1, 1, 1, 1, 0, 0, 1, 1, 1, 0], #5
-    [0, 0, 1, 0, 0, 0, 0, 1, 0, 0], #6
-    [1, 1, 1, 0, 1, 1, 0, 0, 1, 1], #7
-    [1, 1, 1, 0, 1, 1, 0, 0, 1, 0], #8
-    [1, 0, 1, 0, 1, 0, 1, 1, 0, 0], #9
-    [1, 0, 0, 0, 0, 0, 1, 0, 0, 0]  #10
-                                        ])
 
-def initialize_vector(n):
-    return np.zeros((n), dtype=np.int16) #[0 for i in range(n)]
-
+def initialize_vector(pers_vector):
+    return pers_vector / pers_vector.sum()
 
 def calculation_vector():
     a = initialize_vector(4)
@@ -24,12 +10,53 @@ def calculation_vector():
     print(a)
 
 
-def itemRank (A: np.matrix , alpha: float, v: np.array, m: bool): #−> np.array
-
+def itemRank(A: np.matrix , alpha: float, v: np.array, m: bool): #−> np.array
+    P = get_probability_transition_matrix(A)
+    di = initialize_vector(A)
+    if m:
+        item_rank_recursively(P, di, v, )
     return []
+
+def item_rank_recursively(A: np.matrix , alpha: float, v: np.array):
+
+    return
+
+
+def item_rank_inversion_mat(A, pers_vect):
+    print("identity", np.identity(A.__len__()))
+    res = (1 - 0,85)*(1/(np.identity(A.__len__())- 0,85*get_probability_transition_matrix(A).transpose()))*pers_vect
+    print("res", res)
+    return res
+
+
+def get_probability_transition_matrix(A: np.matrix):
+    res = []
+    for i in range(10):
+        res.append([0] * 10)
+    do = A.sum(axis=1)
+    for i in range(A.__len__()):
+        res[i] = (A[i]/float(do.item(i))).A1
+    return np.matrix(res)
 
 
 if __name__ == '__main__':
-    print("Item Rank", initialize_vector(3))
-    calculation_vector()
+    #print("Item Rank", initialize_vector(3))
+    #calculation_vector()
+    matrix = np.matrix([
+        # 1  2  3  4  5  6  7  8  9  10
+        [0, 1, 1, 0, 1, 0, 1, 1, 1, 1],  # 1
+        [1, 0, 0, 1, 1, 0, 1, 1, 0, 0],  # 2
+        [1, 0, 0, 0, 1, 1, 1, 1, 1, 0],  # 3
+        [0, 1, 0, 0, 1, 0, 0, 0, 0, 0],  # 4
+        [1, 1, 1, 1, 0, 0, 1, 1, 1, 0],  # 5
+        [0, 0, 1, 0, 0, 0, 0, 1, 0, 0],  # 6
+        [1, 1, 1, 0, 1, 0, 0, 0, 1, 1],  # 7
+        [1, 1, 1, 0, 1, 1, 0, 0, 1, 0],  # 8
+        [1, 0, 1, 0, 1, 0, 1, 1, 0, 0],  # 9
+        [1, 0, 0, 0, 0, 0, 1, 0, 0, 0]  # 10
+    ])
 
+    personalisation_vector = np.array([1, 0, 0, 0, 1, 0, 1, 0, 0, 1])
+
+    personalisation_vector = initialize_vector(personalisation_vector)
+    item_rank_inversion_mat(matrix, initialize_vector(personalisation_vector))
